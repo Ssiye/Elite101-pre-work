a5ccfd8acfc307a819bff868e5b816ccc590a4e4
Ssiye <154105209+Ssiye@users.noreply.github.com>
2024-01-08 03:03:17 +0000

    Added options + responses for them

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-print("Hello welcome, my name is chtabot.Whats yours?")
-name = input() 
-print("hello nice to meet you " + name + "") 
-print( "How old are you " + name + "?")
-age = input()
-print("What a great age to be," + age + "!!")
-print("So," + name + " what can I do for you.")
-print(" Chose from the folowing options\nOption 1\n Option 2\nEnd Program")
+name = input("Hello welcome, my name is chtabot.Whats yours? ") 
+print("hello nice to meet you " + name + "!! ")
+age = input("How old are you")
+print("Wow" + age + "what a great age to be!! ")
+option = input(" So," + name + " what can I do for you.\nChose from the folowing options\nOption 1\nOption 2\nEnd Conversation")
+if option == "Option 1":
+  print("Option 1")
+elif option == "Option 2":
+  print("Option 2")
+elif option == "End Conversation":
+  print("Good bye" + name + " it was nice talking with you!!")
+else:
+  print("Sorry I dint understand that🥲\nCould you try again?")
+  
