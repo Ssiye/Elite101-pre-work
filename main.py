2aff8868c797e404c56e6a436eb7e570b0867b9e
Ssiye <154105209+Ssiye@users.noreply.github.com>
2024-01-09 18:47:05 +0000

    Convo options now loop after incorect inputs, and each option until End is inputed
    -Added defs, while program_loops, and in_use to alow the code to loop correctly.
    -Also fixed any gramatical/speeling errors+ sligtly modifed some resposes.

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-name = input("Hello welcome, my name is chtabot.Whats yours? ") 
-print("hello nice to meet you " + name + "!! ")
+name = input("Hello welcome, my name is chatbot.101, Whats yours? ") 
+print("hello nice to meet you " + name + ", thats such a nice name!!")
 age = input("How old are you")
-print("Wow" + age + "what a great age to be!! ")
-option = input(" So," + name + " what can I do for you.\nChose from the folowing options\nOption 1\nOption 2\nEnd Conversation")
-if option == "Option 1":
-  print("Option 1")
-elif option == "Option 2":
-  print("Option 2")
-elif option == "End Conversation":
-  print("Good bye" + name + " it was nice talking with you!!")
-else:
-  print("Sorry I dint understand that🥲\nCould you try again?")
-  
+print("Wow " + age + " what a great age to be!! ")
+
+program_loop = True
+
+print("So," + name + " what can I do for you.")
+def choices():
+  print("\n1.Option 1\n2.Option 2\n3.End Conversation\n")
+
+def choice():
+  in_use = True
+  option = input("Chose from the folowing options by typeing in a number 1-3: ")
+  if option == "1":
+    print("Option 1")
+    return in_use
+  elif option == "2":
+    print("Option 2")
+    return in_use
+  elif option == "3":
+    print("Good bye " + name + ", it was nice talking with you!!")
+    in_use = False 
+  else: 
+    print("Sorry I didn't understand that 🥲\nCould you try again?\n")
+    return in_use
+while program_loop: 
+  choices()
+  program_loop = choice()
