044b62224e0b81c326a7f3baaae3e71e9f31816a
Ssiye <154105209+Ssiye@users.noreply.github.com>
2024-01-09 21:04:06 +0000

    Added colons and spaces at the ends of questions

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-name = input("Hello welcome, my name is chatbot.101, Whats yours? ") 
-print("hello nice to meet you " + name + ", thats such a nice name!!")
-age = input("How old are you")
+name = input("Hello welcome, my name is chatbot.101, Whats yours? :  ") 
+print("Hello nice to meet you " + name + ", thats such a nice name!!")
+age = input("How old are you? :  ")
 print("Wow " + age + " what a great age to be!! ")
-
+#convo menu options
 program_loop = True
-
-print("So," + name + " what can I do for you.")
+print("\nSo," + name + " what can I do for you?: ")
 def choices():
-  print("\n1.Option 1\n2.Option 2\n3.End Conversation\n")
+  print("1.Option 1\n2.Option 2\n3.End Conversation\n")
 
 def choice():
   in_use = True
@@ -22,7 +21,7 @@ def choice():
     print("Good bye " + name + ", it was nice talking with you!!")
     in_use = False 
   else: 
-    print("Sorry I didn't understand that 🥲\nCould you try again?\n")
+    print("\nSorry I didn't understand that 🥲\nCould you try again?\n")
     return in_use
 while program_loop: 
   choices()
